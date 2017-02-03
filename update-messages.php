3b027f61fa673d553a37a743c68f2fd144ade791
Ashish Adhav <aashish.aadhav@gmail.com>
2017-02-03 17:05:38 -0500

    added template and update message

--- update-messages.php
+++ update-messages.php
@@ -4,16 +4,23 @@
 $db = new mysqli("localhost","root","zeroarch","msg" );
 
 if ( $db->connect_error) {
-
 	die("Could not connect to DB");
-
-} else {
-
-	die("connection successful");
-
 }
 
 
+
+$username= stripslashes(htmlspecialchars($_GET['username']));
+$message=stripslashes(htmlspecialchars($_GET['message']));
+
+	if ( $username == "" || $message == "" ) {
+		die("null username passwd");
+	}
+
+	
+	$result= $db->prepare("INSERT INTO messages values('',?,?)") ;
+	$result->bind_param("ss",$username,$message);
+	$result->execute();
+	
 
 ?>
 
